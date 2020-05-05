1135596476208d5991733f1288669caa157359bb
Sanduni Nagahage <sanduninagahage@gmail.com>
2020-05-05 20:37:55 +0530

    completed Update Operation

--- src/main/java/com/example/tutorialapp/controller/TutorialController.java
+++ src/main/java/com/example/tutorialapp/controller/TutorialController.java
@@ -54,7 +54,18 @@ public class TutorialController {
     
     @PutMapping("/tutorials/{id}")
         public ResponseEntity<Tutorial> updateTutorial(@PathVariable ("id") String id, @RequestBody Tutorial tutorial){
+            Optional<Tutorial> tutorialData=tutorialRepository.findById(id);
 
+            if(tutorialData.isPresent()){
+                Tutorial _tutorial=tutorialData.get();
+                _tutorial.setTitle(tutorial.getTitle());
+                _tutorial.setDescription(tutorial.getDescription());
+                _tutorial.setPublished(tutorial.isPublished());
+                return new ResponseEntity<>(tutorialRepository.save(_tutorial),HttpStatus.OK)
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         }
 
     @DeleteMapping("/tutorials/{id}")
