2cb68b8c07d865627992b579ab18876b26424623
Sanduni Nagahage <sanduninagahage@gmail.com>
2020-05-05 09:32:52 +0530

    Completed the Retrieve Operation

--- src/main/java/com/example/tutorialapp/controller/TutorialController.java
+++ src/main/java/com/example/tutorialapp/controller/TutorialController.java
@@ -32,7 +32,14 @@ public class TutorialController {
 
     @GetMapping("/tutorials/{id}")
         public ResponseEntity<List<Tutorial>> getTutorialsById(@PathVariable("id") String id){
+            Optional<Tutorial> tutorialData=tutorialRepository.findById(id);
 
+            if(tutorialData.isPresent()){
+                return new ResponseEntity<>(tutorialData.get(),HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         }
 
     @PostMapping("/tutorials")
@@ -62,6 +69,15 @@ public class TutorialController {
 
     @GetMapping("/tutorials/published")
         public ResponseEntity<List<Tutorial>> findByPublished(){
+            try {
+                List<Tutorial> tutorials= tutorialRepository.findByPublished(true);
 
+                if(tutorials.isEmpty()){
+                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                }
+                return new ResponseEntity<>(tutorials,HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+            }
         }
 }
